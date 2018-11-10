74d7b4caad5a16c6ae9a95302c0eb0a5dca43db2
Sauro <blumenau.andre@gmail.com>
2018-11-10 02:43:56 -0200

    #NOT EXPLODING RAM

--- RDFM_LOCAL_MOVIELENS.py
+++ RDFM_LOCAL_MOVIELENS.py
@@ -50,7 +50,9 @@ def sgd_subset(train_X, train_Y, iterations, alpha, regularization,weight_matrix
 
     error_iter_array = numpy.tile(1,(iterations,1))     
 
+    print("iterations = ",iterations)
     for i in range(iterations):
+        print("iter = ",i)
         seed = seed + 1
         numpy.random.seed(seed)
         random_idx_list = numpy.random.permutation(N)
@@ -72,6 +74,8 @@ def sgd_subset(train_X, train_Y, iterations, alpha, regularization,weight_matrix
             tensor_of_proto_square = numpy.tensordot(tensor_of_x_features_squared[idxs],weight_matrix_square,axes=1)
             vector_of_prediction = numpy.tensordot(((tensor_of_proto_vx*tensor_of_proto_vx) - tensor_of_proto_square),vector_of_sum,axes=1).sum(axis=1)*0.5
             b = train_Y[idxs]-vector_of_prediction           
+            
+            print(b.mean())
    
             error_sum = error_sum+b.mean()
             
@@ -89,15 +93,17 @@ def sgd_subset(train_X, train_Y, iterations, alpha, regularization,weight_matrix
 
         #error_iter_array[i] = error_sum/splits
 
-        if numpy.abs(numpy.abs(error_iter_array[i]) - last_iteration_error) < 0.0000001:
-            patience = patience+1
-        else:
-            patience = 0
+        # if numpy.abs(numpy.abs(error_iter_array[i]) - last_iteration_error) < 0.0000001:
+            # print("0.0000001")
+            # print("error = ",(numpy.abs(error_iter_array[i]) - last_iteration_error)[0])
+            # patience = patience+1
+        # else:
+            # patience = 0
 
-        if patience == patience_limit:
-            break
+        # if patience == patience_limit:
+            # break
 
-        last_iteration_error = numpy.abs(error_iter_array[i])
+        # last_iteration_error = numpy.abs(error_iter_array[i])
         
     return weight_matrix,error_iter_array.mean()
 
@@ -114,22 +120,7 @@ def fm_gradient_sgd_trick(x_features, y_target, weights, regularization,meio,pro
 def delete_column(array, *args):
     filtered_names = [x for x in array.dtype.names if x not in args]
     return array[filtered_names]
-
-def softmax(X,W,Y_VECTOR_EXPIT_SUM):
-    xa = numpy.array([X])
-    VX =  xa.dot(W)
-    VX_square = (xa*xa).dot(W*W)
-    phi = 0.5*(VX*VX - VX_square).sum()
-    
-    return phi
-    #z = [0.0, 1.0]
-    #softmax_result = scipy.special.expit(phi)/numpy.sum(scipy.special.expit(z))
-    #if softmax_result > 0.5:
-    #    return 1
-    #else:
-    #    return 0
-        
-    
+   
 def fm_get_p(X, W):
     #print(W)
     xa = numpy.array([X])
@@ -137,17 +128,23 @@ def fm_get_p(X, W):
     VX_square = (xa*xa).dot(W*W)
     phi = 0.5*(VX*VX - VX_square).sum()
 
-    if phi > 0.5:
-        return 1
-    else:
-        return 0
+    return phi
         
 def table_adapted(X,Y):
+
+    X = numpy.round(X,1)*5
+    Y = Y*5
+    Y = [item for sublist in Y for item in sublist]
+    
+    #Y = Y*5
+    print(X[1:10].tolist())
+    print(Y[1:10])
     w, h = 2, 6
     table_t = [[0 for x in range(w)] for y in range(h)]
     X = numpy.array(X)
     X = numpy.clip(X,0,5)
     X = X.tolist()
+    
     
     for i in range(len(X)):
         if X[i] == 0:
@@ -180,29 +177,16 @@ def table_adapted(X,Y):
     print(table_t)
     return table_t
         
-        
-def table(X,Y):
-    w, h = 2, 2
-    table_t = [[0 for x in range(w)] for y in range(h)]
-    for i in range(len(X)):        
-        a = (0 if X[i] < 0.5 else 1)
-        b = (0 if Y[i] < 0.5 else 1)
-        table_t[a][b] = table_t[a][b] + 1
-    print(table_t)
-    return table_t
-
 def evaluate(x, y, w):
     print('evaluation')
     print('min y', min(y))
     print('max y', max(y))
     p_y = []
-    
-    y_expit=numpy.sum(scipy.special.expit(y))
 
     for i in range(x.shape[0]): 
-        p_y.append(round(softmax(x[i], w,y_expit)*5))#,meio))
-        
-    perf = table_adapted(p_y, y*5)
+        p_y.append(fm_get_p(x[i], w))
+
+    perf = table_adapted(p_y, y)
     print('Performance: ', perf)
     print('Accuracy:',(perf[0][0]+perf[1][1])/x.shape[0])
     print('MATTHEWS Coefficient:',MatthewsCoefficient(perf))
@@ -213,7 +197,7 @@ def MatthewsCoefficient(perf_table):
     fp = perf_table[0][1] #false positive
     fn = perf_table[1][0] #false negative
     
-    M = (tp*tn - (fp*fn))/numpy.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
+    M = (tp*tn - (fp*fn))/(numpy.sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))+0.00000001)
     return M
 
 path_csv = "C:\PosGrad\Movielens1M\data_processed_ 1 .csv"
@@ -222,11 +206,11 @@ target_column = "Rating"
 
 trainX,trainY,validationX,validationY = process_csv_data(path_csv, 0,1000,delimiter,target_column)
 
-a_factors = 4
+a_factors = 5
 
 skip = 0
 end = 0
-sp_split = 100 #Split for Memory
+sp_split = 500 #Split for Memory
 take = numpy.floor(trainX.shape[0]/sp_split).astype(numpy.int32)
 start = time.time()
 
@@ -238,10 +222,11 @@ sp_patience = 0
 sp_patience_limit = 80
 sp_last_iteration_error = 0
 
-for i in range(sp_split):    
+#for i in range(sp_split):    
+for i in range(1):
     skip = i*take    
     end = ((i+1)*take)      
-    modelo,sp_error = sgd_subset( trainX[skip:end], trainY[skip:end], iterations=1, alpha=1/(100), regularization=1/(1000), weight_matrix=modelo)
+    modelo,sp_error = sgd_subset( trainX[skip:end], trainY[skip:end], iterations=20, alpha=1/(100), regularization=1/(1000), weight_matrix=modelo)
 
     if numpy.abs(numpy.abs(sp_error)-sp_last_iteration_error) < 0.0000001:
         sp_patience = sp_patience+1
