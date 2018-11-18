c479bd557a6ad701b4b11a2557c2124dc17a2919
Sauro <blumenau.andre@gmail.com>
2018-11-18 10:59:44 -0200

    ##################

--- RDFM_LOCAL_MOVIELENS.py
+++ RDFM_LOCAL_MOVIELENS.py
@@ -13,12 +13,6 @@ from metrics import evaluate
 from cpu_learning import optimize
 import csv
 import gc
-   
-path_csv = "C:\PosGrad\Movielens1M\data_processed_ 1 .csv"
-delimiter  = ","
-target_column = "Rating"
-
-trainX,trainY,validationX,validationY = process_csv_data(path_csv, 0,1000,delimiter,target_column)
 
 class FactorizationMachine:
     self.model = None
@@ -37,16 +31,19 @@ class FactorizationMachine:
     self.memory_split=0
 
     def get_random_weight_matrix(number_of_features,number_of_latent_vectors):
-        #modelo =  numpy.random.ranf((trainX.shape[1], number_of_latent_vectors))
-        modelo =  numpy.random.ranf((number_of_features, number_of_latent_vectors))
-        modelo = modelo / numpy.sqrt((modelo*modelo).sum())
-        return modelo
+        #model =  numpy.random.ranf((trainX.shape[1], number_of_latent_vectors))
+        model =  numpy.random.ranf((number_of_features, number_of_latent_vectors))
+        model = model / numpy.sqrt((model*model).sum())
+        return model
     
     def function(self,iterations,learning_rate,latent_vectors,regularization,slice_size,batch_size,
-    slice_patience,iteration_patience,iteration_patience_threshold,slice_patience_threshold):    
+    slice_patience,iteration_patience,slice_patience_threshold,iteration_patience_threshold):    
 
         if slice_size < batch_size:
-            raise ValueError('slice_size parameter cannot be smaller than batch_size parameter.')
+            raise ValueError('"slice_size" parameter cannot be smaller than "batch_size" parameter.')
+            
+        if iteration_patience >= iterations:
+            raise ValueError('"iteration_patience" parameter cannot be smaller than "iterations" parameter.')                      
 
         #"Private" properties
         self.model = None
@@ -60,19 +57,17 @@ class FactorizationMachine:
         self.batch_size                     = batch_size                  
         self.slice_patience                 = slice_patience             
         self.iteration_patience             = iteration_patience              
-        self.iteration_patience_threshold   = iteration_patience_threshold
         self.slice_patience_threshold       = slice_patience_threshold   
+        self.iteration_patience_threshold   = iteration_patience_threshold
+
         
         #Computed properties
         
-        ###TODO
-        #How many parts the will be, depends on "slice_size"
-        #self.memory_split =  numpy.floor(trainX.shape[0]/slice_size).astype(numpy.int32)
-
+        #self.slice_count =  numpy.floor(trainX.shape[0]/slice_size).astype(numpy.int32)
         
     # iteration_error = 0 
 
-    # sample_error_limit = 0.0000001
+    # slice_patience_threshold = 0.0000001
     # patience_counter = 0
     # slice_patience = 80
     
@@ -91,22 +86,23 @@ class FactorizationMachine:
         iteration_error = 0
         last_iteration_error = 0
     
-        slice_count =  numpy.floor(trainX.shape[0]/slice_size).astype(numpy.int32)
+        slice_count =  numpy.floor(trainX.shape[0]/self.slice_size).astype(numpy.int32)
+        
+        if self.slice_patience >= slice_count:
+            raise ValueError('"slice_size" parameter cannot be smaller than "batch_size" parameter.')            
         
         #start = time.time()
-
-        
             
         if model is None:
             self.model = get_random_weight_matrix(trainX.shape[1],self.latent_vectors)
             
-        if model.slice_size  >
+        #if model.slice_size  >
 
         #for j in range(slice_count):        
         for j in range(1):
             skip = j*self.slice_size    
             end = ((j+1)*self.slice_size)      
-            modelo,iteration_error,error_iter_array = optimize( 
+            self.modelo,iteration_error,error_iter_array = optimize( 
                 trainX[skip:end], 
                 trainY[skip:end], 
                 iterations                   = self.iterations,
@@ -117,12 +113,12 @@ class FactorizationMachine:
                 iteration_patience           = self.iteration_patience,            
                 iteration_patience_threshold = self.iteration_patience_threshold)
         
-            if numpy.abs(numpy.abs(iteration_error)-last_iteration_error) < sample_error_limit:
+            if numpy.abs(numpy.abs(iteration_error)-last_iteration_error) < self.slice_patience_threshold:
                 patience_counter = patience_counter+1
             else:
                 patience_counter = 0
                 
-            if patience_counter == slice_patience:
+            if patience_counter == self.slice_patience:
                 break;
             
             last_iteration_error = numpy.abs(iteration_error)
@@ -137,17 +133,25 @@ class FactorizationMachine:
     
 #end = time.time()
 
-        # modelo,iteration_error,error_iter_array = optimize( 
-            # trainX[skip:end], 
-            # trainY[skip:end], 
-            # iterations=5, alpha=1/(100),
-            # regularization=1/(1000),
-            # weight_matrix=modelo,
-            # iteration_patience=10,
-            # batch_size=2,
-            # iteration_patience_threshold=0.0000001)
+var factorization_machine = FactorizationMachine(
+    iterations                      = 10,
+    learning_rate                   = 1/(100),
+    latent_vectors                  = 4,
+    regularization                  = 1/(1000),
+    slice_size                      = 2,
+    batch_size                      = 2,
+    slice_patience                  = 5,
+    iteration_patience              = 5,
+    slice_patience_threshold        = 0.0000001,
+    iteration_patience_threshold    = 0.0000001)
+            
+path_csv = "C:\PosGrad\Movielens1M\data_processed_ 1 .csv"
+delimiter  = ","
+target_column = "Rating"
 
-
+trainX,trainY,validationX,validationY = process_csv_data(path_csv, 0,1000,delimiter,target_column)            
+factorization_machine.learn()
+            
 #print((end - start)," Seconds")
 #print(((end - start)/60)," Minutes")
 evaluate(validationX,validationY,modelo)
